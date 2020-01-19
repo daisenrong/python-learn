9525022f6f0df496f8397a1bfd2feefb572276dc
daisenrong <daisenrong@guazi.com>
2020-01-19 23:05:37 +0800

    homework

--- com/python/learn/basic/Test.py
+++ com/python/learn/basic/Test.py
@@ -1,13 +1,3 @@
-def sumCount(str):
-    result = {}
-    s = ""
-    for i in str.split(" "):
-        if i in result:
-            result[i] = result[i] + 1
-        else:
-            result[i] = 1
-    return result
+di = {" ":1,"2":2}
 
-
-s = sumCount("New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.")
-print(s)
+print(di)
